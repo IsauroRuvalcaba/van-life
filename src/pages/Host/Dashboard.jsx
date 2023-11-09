1fbd3a6cdb860f183019bc590face621063ce07a
Isauro Ruvalcaba <isauroruvalcaba@sbcglobal.net>
2023-11-08 18:51:17 -0800

    created the HostLayout componenet

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -5,9 +5,6 @@ export default function Dashboard() {
   return (
     <>
       <div>This is the Dashboard</div>
-      <Link to="/host/income">Income</Link>
-      <Link to="/host/reviews">Reviews</Link>
-      <Outlet />
     </>
   );
 }
