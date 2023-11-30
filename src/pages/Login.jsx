faf2ac9e1b2bf328db3e4c756f23638d34f393ec
Isauro Ruvalcaba <isauroruvalcaba@sbcglobal.net>
2023-11-29 22:23:34 -0800

    setting up auth using sad path, this is only to practice was done before and with actions it will be different. Added a status state with default as idle and this was to disable the submit button. Added error state and catch to the promise to show the error message on page. also added the finally to the promise to setStatus back to idle.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLoaderData, useNavigate } from "react-router-dom";
 import { loginUser } from "../api";
 
@@ -13,12 +13,26 @@ export default function Login() {
     password: "",
   });
 
+  const [status, setStatus] = useState("idle");
+  const [error, setError] = useState(null);
+
   const message = useLoaderData();
+
+  // useEffect(() => {
+  //   console.log(status);
+  // }, [status]);
 
   function handleSubmit(e) {
     e.preventDefault();
-    // console.log(loginFormData);
-    loginUser(loginFormData).then((data) => console.log(data));
+    setStatus("submitting");
+    setError(null);
+
+    loginUser(loginFormData)
+      .then((data) => console.log(data.user))
+      .catch((err) => setError(err))
+      .finally(() => setStatus("idle")); //this is needed so not stuck in "logging in" if err
+
+    setStatus("idle");
   }
 
   function handleChange(e) {
@@ -32,7 +46,10 @@ export default function Login() {
   return (
     <div className="login-container">
       <h1>Sign in to your account</h1>
+
       {message && <h3 className="red">{message}</h3>}
+      {error && <h3 className="red">{error.message}</h3>}
+
       <form onSubmit={handleSubmit} className="login-form">
         <input
           name="email"
@@ -48,8 +65,28 @@ export default function Login() {
           placeholder="Password"
           value={loginFormData.password}
         />
-        <button>Log in</button>
+        <button disabled={status === "submitting"}>
+          {status === "submitting" ? "Logging in..." : "Log in"}
+        </button>
       </form>
     </div>
   );
 }
+
+//! the error message comes from the api.js loginUser func that throws a message if response not ok. That error was setup in the server.js where if foundUser method returns false it responds with 401 error saying the message.
+
+//https://chat.openai.com/share/b7884c3b-f52a-44ac-8be7-2134ba3a3494
+
+/*
+async function handleSubmit(e) {
+    e.preventDefault();
+    await setStatus("submitting");
+
+    console.log(status);
+    await loginUser(loginFormData).then((data) => console.log(data.user));
+
+    // setStatus("idle");
+    setStatus("idle");
+    console.log(status);
+  }
+*/
