5e859c412a163a7e0e1001d49bcf147c4239c0cb
Isauro Ruvalcaba <isauroruvalcaba@sbcglobal.net>
2023-12-01 15:18:34 -0800

    moved login User func to the action func. Removed login useState, the handleChange func and, onChange and value attributes from input in Form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,17 +7,18 @@ export function loader({ request }) {
   return new URL(request.url).searchParams.get("message");
 }
 
-export async function action() {
-  console.log("Action function");
+export async function action({ request }) {
+  const formData = await request.formData();
+  // get method pulls data from the name attribute of the input in Form
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const data = await loginUser({ email: email, password: password });
+
+  console.log(data);
   return null;
 }
 
 export default function Login() {
-  const [loginFormData, setLoginFormData] = React.useState({
-    email: "",
-    password: "",
-  });
-
   const [status, setStatus] = useState("idle");
   const [error, setError] = useState(null);
 
@@ -35,14 +36,6 @@ export default function Login() {
       .finally(() => setStatus("idle")); //this is needed so not stuck in "logging in" if err
   }
 
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setLoginFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  }
-
   return (
     <div className="login-container">
       <h1>Sign in to your account</h1>
@@ -51,20 +44,8 @@ export default function Login() {
       {error && <h3 className="red">{error.message}</h3>}
 
       <Form method="post" className="login-form">
-        <input
-          name="email"
-          onChange={handleChange}
-          type="email"
-          placeholder="Email address"
-          value={loginFormData.email}
-        />
-        <input
-          name="password"
-          onChange={handleChange}
-          type="password"
-          placeholder="Password"
-          value={loginFormData.password}
-        />
+        <input name="email" type="email" placeholder="Email address" />
+        <input name="password" type="password" placeholder="Password" />
         <button disabled={status === "submitting"}>
           {status === "submitting" ? "Logging in..." : "Log in"}
         </button>
