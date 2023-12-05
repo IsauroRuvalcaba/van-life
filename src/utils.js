6310c4bb09299b79a782169e26d3635ae9e18e18
Isauro Ruvalcaba <isauroruvalcaba@sbcglobal.net>
2023-12-04 22:47:22 -0800

    added to local storage if user is logged in

--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,8 @@ import { redirect } from "react-router-dom";
 
 export async function requireAuth() {
   // this could be checking an external DB for authenticity so it could be asyncronous
-  const isLoggedIn = true;
-
+  const isLoggedIn = localStorage.getItem("loggedin");
+  // const isLoggedIn = true;
   if (!isLoggedIn) {
     throw redirect("/login?message=You must log in first");
   }
